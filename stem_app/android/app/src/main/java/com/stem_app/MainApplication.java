4f7141ec39cf05a40adeddc3d8d3dd8decc4ae38
Paul Le <ple@bgprod.com>
2019-06-12 12:11:02 -0500

    added video

--- stem_app/android/app/src/main/java/com/stem_app/MainApplication.java
+++ stem_app/android/app/src/main/java/com/stem_app/MainApplication.java
@@ -3,6 +3,7 @@ package com.stem_app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
             new ReactVideoPackage(),
             new RNGestureHandlerPackage()
       );
